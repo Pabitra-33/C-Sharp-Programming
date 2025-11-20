a26ff71700d88dc8f975a278f9e9c80ae68a5984
Pabitra-33 <kumarpabitra345@gmail.com>
2025-11-20 06:38:11 +0530

    first commit

---
+++ Basics/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Basics
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
