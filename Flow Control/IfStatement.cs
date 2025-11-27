703824a25a3e841d9bddb0a976e24d91d765732b
Pabitra-33 <kumarpabitra345@gmail.com>
2025-11-27 21:31:08 +0530

    conditional if statement to execute the statement condition

---
+++ Flow Control/IfStatement.cs
@@ -0,0 +1,17 @@
+using System;
+
+namspace FlowControl
+{
+    class IfStatement
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+
+            if (number > 0)
+            {
+                Console.WriteLine("The number is positive.");
+            }
+        }
+    }
+}
