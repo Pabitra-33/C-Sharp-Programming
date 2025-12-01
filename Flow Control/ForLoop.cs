fd3440d7cadff22bc37b2072f0fd60ca19501ddb
Pabitra-33 <kumarpabitra345@gmail.com>
2025-12-01 20:55:17 +0530

    for loop is used when we know the number of iteration

---
+++ Flow Control/ForLoop.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Flow_Control
+{
+    class ForLoop
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Iteration: " + i);
+            }
+        }
+    }
+}
