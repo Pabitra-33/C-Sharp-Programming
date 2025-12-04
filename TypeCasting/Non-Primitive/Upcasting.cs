14c919881d601cd20a4dc4816d798ac997b34f18
Pabitra-33 <kumarpabitra345@gmail.com>
2025-12-04 22:10:26 +0530

    upcasting in c# programming

---
+++ TypeCasting/Non-Primitive/Upcasting.cs
@@ -0,0 +1,37 @@
+using System;
+
+nampespace NonPrimitiveTypeCasting
+{
+    // Base class
+    class Animal
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Animal speaks");
+        }
+    }
+
+    // Derived class
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Dog barks");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an instance of the derived class
+            Dog myDog = new Dog();
+
+            // Upcasting: Implicitly converting derived class to base class
+            Animal myAnimal = myDog;
+
+            // Calling method from the base class
+            myAnimal.Speak();
+        }
+    }
+}
