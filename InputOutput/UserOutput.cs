dd8b770a51e580d9b419c64156c4c1676eaab493
Pabitra-33 <kumarpabitra345@gmail.com>
2025-12-07 12:18:11 +0530

    taking input and outputed the data in the console

---
+++ InputOutput/UserOutput.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InputOutput
+{
+    class UserOutput
+    {
+        Console.Write("Enter Your name: ");//Console.Write() is used to print something in the console and stays in the same line for new line we use Console.WriteLine().
+        string name = Console.ReadLine();
+        
+    }
+}
