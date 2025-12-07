2a57fe94b9075629293c16e8fe4747c00addef97
Pabitra-33 <kumarpabitra345@gmail.com>
2025-12-07 12:22:19 +0530

    taken user inout and performing some validation given the outpt on the console

--- InputOutput/UserOutput.cs
+++ InputOutput/UserOutput.cs
@@ -7,5 +7,15 @@ namespace InputOutput
         Console.Write("Enter Your name: ");//Console.Write() is used to print something in the console and stays in the same line for new line we use Console.WriteLine().
         string name = Console.ReadLine();
         
+        Console.WriteLine($"Your name is: {name}");
+        Console.WriteLine("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if(age < 18){
+            Console.WriteLine("You are not eligible for vote. Your age is too low!");
+        }
+        else{
+            Console.WriteLine("You are eligible for vote");
+        }
     }
 }
