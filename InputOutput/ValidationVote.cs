b60a5aa26a99e2df0418eb33b757e196b23f2c78
Pabitra-33 <kumarpabitra345@gmail.com>
2025-12-09 22:30:37 +0530

    taken the age input and provided the voting validation

--- InputOutput/ValidationVote.cs
+++ InputOutput/ValidationVote.cs
@@ -5,7 +5,9 @@ namespace InputOutput
     class ValidationVote
     {
         static void Main(){
-            int var = "Hello I am ";
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.Int32(Console.ReadLine());
+            Console.WriteLine(age >= 18 ? "Eligible to vote":"Not Eligible to vote");
         }
     }
 }
