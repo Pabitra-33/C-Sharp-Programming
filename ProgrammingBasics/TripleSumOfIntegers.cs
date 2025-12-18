133941676e5ef1d6248ad274d9fc081b563a2580
Pabitra-33 <kumarpabitra345@gmail.com>
2025-12-18 22:19:40 +0530

    same or triplet sum of two integers

---
+++ ProgrammingBasics/TripleSumOfIntegers.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ProgrammingBasics
+{
+    class TripleSumOfIntegers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first integer: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second integer: ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(TripleSum(input1, input2));
+        }
+        public static int SumTriple(int a, int b)
+        {
+            return a == b ? (a + b) * 3 : a + b; // If 'a' equals 'b', return the triple sum of 'a' and 'b', otherwise return their sum
+        }
+    }
+}
