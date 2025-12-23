cc97434f847bbe2d45209c9f5d9d19bd4bd2b14e
Pabitra-33 <kumarpabitra345@gmail.com>
2025-12-23 21:34:31 +0530

    added some comments to understand

--- ProgrammingTasks/StringLowercase.cs
+++ ProgrammingTasks/StringLowercase.cs
@@ -4,6 +4,7 @@ namespace ProgrammingTasks
 {
     class StringLowercase
     {
+        // Main method
         static void Main(string[] args) {
           Console.WriteLine("\nInput a string:"); 
           string input = Console.ReadLine();
@@ -11,6 +12,7 @@ namespace ProgrammingTasks
           Console.WriteLine(result(input));
         }
         
+        //convert a string to lowercase
         public static string result(string str) {
             return str.ToLower();
         }
