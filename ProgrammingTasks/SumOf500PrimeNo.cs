695e50c6f5ca694dc8b5aa04084621b00e0ce92e
Pabitra-33 <kumarpabitra345@gmail.com>
2025-12-25 09:19:19 +0530

    added some new comments and modifiied some coding standards

--- ProgrammingTasks/SumOf500PrimeNo.cs
+++ ProgrammingTasks/SumOf500PrimeNo.cs
@@ -10,7 +10,7 @@ namespace ProgrammingTasks
         int counter = 0; 
         int n = 2; // Starting from the first prime number
 
-        // Loop to find and sum the first 500 prime numbers
+        // Looping to find and sum the first 500 prime numbers
         while (counter < 500)
         {
             if (isPrime(n)) // Checking if 'n' is a prime number by calling the 'isPrime' method
@@ -21,21 +21,21 @@ namespace ProgrammingTasks
             n++; // Moving to the next number for evaluation
         }
 
-        Console.WriteLine(sum); // Displaying the sum of the first 500 prime numbers
+        Console.WriteLine(sum); // Displaying the sum
     }
 
     // Method to check if a number is prime
-    public static bool isPrime(int n)
+    public static bool isPrime(int num)
     {
-        int x = (int)Math.Floor(Math.Sqrt(n)); // Calculating the square root of 'n'
+        int root = (int)Math.Floor(Math.Sqrt(num)); // Calculating the square root of 'num'
 
-        if (n == 1) return false; // 1 is not a prime number
-        if (n == 2) return true; // 2 is a prime number
+        if (num == 1) return false; // 1 is not a prime number
+        if (num == 2) return true; // 2 is a prime number
 
-        // Loop to check if 'n' is divisible by any number from 2 to square root of 'n'
-        for (int i = 2; i <= x; ++i)
+        // Loop to check if 'num' is divisible by any number from 2 to square root of 'num'
+        for (int i = 2; i <= root; ++i)
         {
-            if (n % i == 0) return false; // If 'n' is divisible by 'i', it's not a prime number
+            if (num % i == 0) return false; // If 'num' is divisible by 'i', it's not a prime number
         }
 
         return true; // 'n' is prime if not divisible by any number except 1 and itself
