8a942e17f55794e4b5e703d4e8191b418959d6e7
Pabitra-33 <kumarpabitra345@gmail.com>
2026-01-04 21:47:41 +0530

    checking a user input will greater than or not

---
+++ ProgrammingTasks/NumberLessThan100AndGreaterThan200.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ProgrammingTasks
+{
+    oublic class NumberLessThan100AndGreaterThan200
+    {
+        public static bool IsNumberLessThan100OrGreaterThan200(int number)
+        {
+            return number < 100 || number > 200;
+        }
+
+        //main method
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool result = IsNumberLessThan100OrGreaterThan200(number);
+            Console.WriteLine($"Is the number less than 100 or greater than 200? {result}");
+        }
+    }
+}
