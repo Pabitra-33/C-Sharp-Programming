a874909dc67a5f7567a449a00789b68bdc17c6ba
Pabitra-33 <kumarpabitra345@gmail.com>
2026-01-08 22:22:48 +0530

    finding the absolute difference between a range acc to 20

--- ProgrammingTasks/AbsoluteDifferenceEquals.cs
+++ ProgrammingTasks/AbsoluteDifferenceEquals.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace ProgrammingTasks
 {
+    // checking if the absolute difference between n and 100 or 200 is within 20
     class AbsoluteDifferenceEquals
     {
         static void Main(string[] args) {
@@ -11,9 +12,10 @@ namespace ProgrammingTasks
           Console.WriteLine(result(x));
         }
         
+        //method to check the absolute difference condition
         public static bool result(int n) {
             if (Math.Abs(n - 100) <= 20 || Math.Abs(n - 200) <= 20){
-                return true;
+                return true;//returning true if condition is met
             }
          return false;
         }
