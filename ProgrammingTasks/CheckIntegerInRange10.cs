27a4632f65be7b573a45051adf58c26ee578ede6
Pabitra-33 <kumarpabitra345@gmail.com>
2026-01-09 22:05:38 +0530

    added a comment

--- ProgrammingTasks/CheckIntegerInRange10.cs
+++ ProgrammingTasks/CheckIntegerInRange10.cs
@@ -16,6 +16,7 @@ namespace ProgrammingTasks
             Console.WriteLine("Enter a number:");//asking user input
             int number = Convert.ToInt32(Console.ReadLine());//converting input to integer
 
+            //calling the method to check if number is in range
             bool result = IsNumberInRange10(number);//storing the boolean result
 
             Console.WriteLine($"Is the number in the range of -10 to 10 inclusive? {result}");
