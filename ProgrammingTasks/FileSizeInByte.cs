53d8463cc224eaa92822194151f35d982b409c21
Pabitra-33 <kumarpabitra345@gmail.com>
2026-01-11 12:33:49 +0530

    printing the file size in the byte form

--- ProgrammingTasks/FileSizeInByte.cs
+++ ProgrammingTasks/FileSizeInByte.cs
@@ -5,17 +5,19 @@ namespace ProgrammingTasks
 {
     public class FileSizeInByte
     {
+        // Method to get the size of a file in bytes
         public static long GetFileSizeInBytes(string filePath)
         {
             // Check if the file exists
             if (!File.Exists(filePath))
             {
+                // Throw an exception if the file does not exist
                 throw new FileNotFoundException("The specified file does not exist.", filePath);
             }
 
             // Get the file info and return its size in bytes
             FileInfo fileInfo = new FileInfo(filePath);
-            return fileInfo.Length;
+            return fileInfo.Length;// Return file size in bytes
         }
 
         public static void Main(string[] args)
