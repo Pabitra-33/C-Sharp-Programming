34a47657766966b384611c6567907a11b3dbbd0f
Pabitra-33 <kumarpabitra345@gmail.com>
2026-01-20 22:48:15 +0530

    converting the given string to lowercase using built in function lower function

--- ProgrammingTasks/StringLowercase.cs
+++ ProgrammingTasks/StringLowercase.cs
@@ -2,19 +2,20 @@ using System;
 
 namespace ProgrammingTasks
 {
-    class StringLowercase
+    // This program converts a given string to lowercase
+    public class StringLowercase
     {
         // Main method
         static void Main(string[] args) {
           Console.WriteLine("\nInput a string:"); 
-          string input = Console.ReadLine();
+          string input = Console.ReadLine();//read input from user
 
           Console.WriteLine(result(input));
         }
         
         //convert a string to lowercase
         public static string result(string str) {
-            return str.ToLower();
+            return str.ToLower();//converting to lowercase
         }
     }
 }
