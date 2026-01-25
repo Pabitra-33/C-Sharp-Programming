ac4fdac5d3b7d153bd41535e2913c47356615c53
Pabitra-33 <kumarpabitra345@gmail.com>
2026-01-25 08:10:37 +0530

    to print the sum of integers of a given no

--- ProgrammingTasks/SumOfDigitsOfIntegers.cs
+++ ProgrammingTasks/SumOfDigitsOfIntegers.cs
@@ -2,14 +2,18 @@ using System;
 
 namespace ProgrammingTasks
 {
+    // This program calculates the sum of the digits of a given positive integer N.
     public class SumOfDigitsOfIntegers
     {
+        //main method
         public static void Main(string[] args)
         {
+            // Prompting user for input
             Console.WriteLine("Enter a positive integer N to calculate the sum of the numbers in it:");
+            // Reading input from user
             int num = Convert.ToInt32(Console.ReadLine());
 
-            int sum = 0;
+            int sum = 0;// Variable to store the sum of digits
             while(num != 0)
             {
                 sum += num % 10; // Extracting the last digit and adding it to sum
